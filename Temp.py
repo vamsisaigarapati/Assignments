e4118dc650f3330c817a995369715f3f146102f5
Vamsi Sai Garapati <35292779+vamsisaigarapati@users.noreply.github.com>
2024-01-11 10:27:13 +0530

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,30 +1,23 @@
-def optimal_winner(colors):
+def gameWinner(colors):
     while True:
-        # Wendy's turn
-        wendy_move = None
+        wm = None
         for i in range(1, len(colors) - 1):
             if colors[i] == 'w' and colors[i - 1] == 'w' and colors[i + 1] == 'w':
-                wendy_move = i
+                wm = i
                 break
 
-        if wendy_move is not None:
-            colors = colors[:wendy_move] + colors[wendy_move + 1:]
+        if wm is not None:
+            colors = colors[:wm] + colors[wm + 1:]
         else:
-            return 'bob'  # Wendy can't make a move, Bob wins
+            return 'bob'  
 
-        # Bob's turn
-        bob_move = None
+        bm = None
         for i in range(1, len(colors) - 1):
             if colors[i] == 'b' and colors[i - 1] == 'b' and colors[i + 1] == 'b':
-                bob_move = i
+                bm = i
                 break
 
-        if bob_move is not None:
-            colors = colors[:bob_move] + colors[bob_move + 1:]
+        if bm is not None:
+            colors = colors[:bm] + colors[bm + 1:]
         else:
-            return 'wendy'  # Bob can't make a move, Wendy wins
-
-# Example usage
-colors_example = 'wwwbbbbwww'
-result = optimal_winner(colors_example)
-print(result)
+            return 'wendy'  
